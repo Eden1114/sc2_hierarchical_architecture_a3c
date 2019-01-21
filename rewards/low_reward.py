f7cc195549950b89b6482193f9ebc2ad078a7d6a
AmosTLOU <37543502+AmosTLOU@users.noreply.github.com>
2019-01-21 15:41:20 +0800

    Revert "reward修改"
    This reverts commit f9771b4463cdc8aeae505f6259d59ce141205984.

--- rewards/low_reward.py
+++ rewards/low_reward.py
@@ -3,40 +3,24 @@ def low_reward(next_obs, obs, type_of_action, coordinate):
   reward = 0
   ourside=[20,25]
   enemyside=[52,49]
-
-  build_score_change = next_obs.observation["score_cumulative"][4] - obs.observation["score_cumulative"][4]
-  killed_value_units_change = 10 * (next_obs.observation["score_cumulative"][5] - obs.observation["score_cumulative"][5])
-  killed_value_structures_change = 10 * (next_obs.observation["score_cumulative"][6] - obs.observation["score_cumulative"][6])
-
   #对己方操作
   if type_of_action == 0:
+    reward += 100
     dis = math.sqrt((coordinate[0] - ourside[0]) ** 2 + (coordinate[1] - ourside[1]) ** 2)
-    reward = 1000-dis * 100
-
-    if build_score_change > 0:
-        if build_score_change == 150:
-            reward += 100
-        elif build_score_change == 100:
-            reward += 50
-
-    if reward < -1000:
-        reward = -1000
-    reward = float(reward / 1000)
+    reward -= dis * 10
+    if reward < -100:
+        reward = -100
+    reward = float(reward / 100)
     return reward
 
   #对敌方操作
   if type_of_action == 1:
+    reward += 100
     dis = math.sqrt((coordinate[0]-enemyside[0])**2+(coordinate[1]-enemyside[1])**2)
-    reward -= 1000 - dis * 100
-
-    if killed_value_units_change > 0:
-        reward += killed_value_units_change
-    if killed_value_structures_change > 0:
-        reward += killed_value_structures_change
-
-    if reward < -1000:
-        reward = -1000
-    reward = float(reward / 1000)
+    reward -= dis * 10
+    if reward < -100:
+      reward = -100
+    reward = float(reward / 100)
     return reward
 
 if __name__ == '__main__':
