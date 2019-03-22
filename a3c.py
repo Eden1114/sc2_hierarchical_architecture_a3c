c574eec623d2d2b49e3737183ce3d0816e512e5b
Lamperougeyxy <pmpyxy@live.com>
2019-03-22 22:26:54 +0800

    测试了feature_map和screen-prep之后的输出。开始训练前应打开episode_log并删除无用的np.save和print

--- a3c.py
+++ a3c.py
@@ -28,6 +28,7 @@ class A3C:
         self.sess = sess
         self.minimap = tf.placeholder(tf.float32, [None, prep.minimap_channel(), 64, 64])
         self.screen = tf.placeholder(tf.float32, [None, prep.screen_channel(), 64, 64])
+        print("minimap_channel, screen_channel: ", prep.minimap_channel(), prep.screen_channel())
         # self.info = tf.placeholder(tf.float32, [None, self.action_num])
         # 为了适配宏动作，把所有的info（available_actions）都去除了
 
@@ -113,6 +114,10 @@ class A3C:
         # 以下4行将minimap和screen的特征做一定处理后分别保存在minimap和screen变量中，为了与placeholder匹配，功能未研究
         minimap = prep.preprocess_minimap(state.observation['feature_minimap'])
         screen = prep.preprocess_screen(state.observation['feature_screen'])
+        num_step = GL.get_value(0, "num_steps")
+        if num_step % 20 == 0:
+            np.save("./DataForAnalysis/screen_episode_" + str(num_step) + ".npy", screen)
+            np.save("./DataForAnalysis/feature_screen_episode_" + str(num_step) + ".npy", state.observation['feature_screen'])
         # info = np.zeros([1, self.action_num], dtype=np.float32)
         # info[0, state.observation['available_actions']] = 1  
         feed = {self.minimap: minimap, self.screen: screen}  # self.info: info
@@ -347,7 +352,7 @@ def run(agent, max_episode, map_name, thread_index, flags, snapshot_path):
             state = next_state
 
         # 存储episode数据
-        episode_log(state, episode, thread_index, counter, thread_index_all, flags, snapshot_path, agent)
+        # episode_log(state, episode, thread_index, counter, thread_index_all, flags, snapshot_path, agent)
         if episode > max_episode:
             env.close()
             break
