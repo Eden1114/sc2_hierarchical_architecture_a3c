09cf9236d0738e9e7de54759db153478be5b35d9
Lamperougeyxy <pmpyxy@live.com>
2019-04-02 18:35:02 +0800

    可以正常记录iswin_self了

--- plot.py
+++ plot.py
@@ -36,7 +36,7 @@ Python-Matplotlib 9 颜色和样式
 　　　　kp-
 """
 
-tmp0 = np.load('./DataForAnalysis/victory_or_defeat_thread_4.npy')
+tmp0 = np.load('./DataForAnalysis/victory_or_defeat_self_thread_4_episode_10.npy')
 # tmp1 = np.load('./DataForAnalysis/victory_or_defeat_thread1.npy')
 # tmp2 = np.load('./DataForAnalysis/victory_or_defeat_thread2.npy')
 # tmp3 = np.load('./DataForAnalysis/victory_or_defeat_thread3.npy')
@@ -95,10 +95,11 @@ tmp0 = np.load('./DataForAnalysis/victory_or_defeat_thread_4.npy')
 
 count = 0
 for i in tmp0:
-    if i > -1:
+    if i == 1:
         count += 1
 rate = count / len(tmp0)
 print('count, rate: ', count, rate)
+print(tmp0)
 
 plt.plot(np.arange(len(tmp0)), tmp0, 'b-')
 # plt.plot(np.arange(len(tmp1)), tmp1, 'r-')
